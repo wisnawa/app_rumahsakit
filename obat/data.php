efb4b65ee2ffbc910ac56c112d835432f94da0e0
wisnawa <putu.wisnawa@gmail.com>
2023-02-17 16:24:46 +0800

    modified code and writing in line code data.php

--- obat/data.php
+++ obat/data.php
@@ -9,8 +9,28 @@
     </div>
 </div>
 <div class="container">
+    <div class="row">
+        <div class="col-6">
+            <form action="" method="post">
+                <div class="input-group mb-3">
+                    <input type="text" name="pencarian" class="form-control" placeholder="Pencarian" aria-label="Pencarian" aria-describedby="button-addon1">
+                    <button class="btn btn-outline-primary" type="submit" id="button-addon1"><i class="fa-solid fa-magnifying-glass"></i></button>
+                </div>
+            </form>
+        </div>
+        <div class="col-6">
+            <!-- button process start -->
+            <div class="d-grid gap-3 d-md-flex justify-content-md-end mb-2">
+                <a href="#" class="btn btn-sm btn-outline-warning" type="reset"><i class="fa-solid fa-arrows-rotate"></i>&nbsp;Refresh Data</a>
+                <a href="#" class="btn btn-sm btn-outline-success"><i class="fa-regular fa-paper-plane"></i>&nbsp;Tambah Data Obat</a>
+            </div>
+            <!-- button process end -->
+        </div>
+    </div>
+</div>
+<div class="container">
     <div class="table-responsive-sm">
-        <table class="table table-striped table-hover caption-top">
+        <table class="table table-striped table-hover">
             <caption>List Obat</caption>
             <thead class="table-primary">
                 <tr>
@@ -22,8 +42,32 @@
             </thead>
             <tbody>
                 <?php
+                $batas = 5;
+                $hal = @$_GET['hal'];
+                if (empty($hal)) {
+                    $posisi = 0;
+                    $hal = 1;
+                } else {
+                    $posisi = ($hal - 1) * $batas;
+                };
                 $no = 1;
-                $sql_obat = mysqli_query($con, "SELECT * FROM tb_obat") or die(mysqli_error($con));
+                if ($_SERVER['REQUEST_METHOD'] == "POST") {
+                    $pencarian = trim(mysqli_real_escape_string($con, $_POST['pencarian']));
+                    if ($pencarian != '') {
+                        $sql = "SELECT * FROM tb_obat WHERE nama_obat LIKE '%$pencarian%'";
+                        $query = $sql;
+                        $queryJml = $sql;
+                    } else {
+                        $query = "SELECT * FROM tb_obat LIMIT $posisi, $batas";
+                        $queryJml = "SELECT * FROM tb_obat";
+                        $no = $posisi + 1;
+                    }
+                } else {
+                    $query = "SELECT * FROM tb_obat LIMIT $posisi, $batas";
+                    $queryJml = "SELECT * FROM tb_obat";
+                    $no = $posisi + 1;
+                };
+                $sql_obat = mysqli_query($con, $query) or die(mysqli_error($con));
                 if (mysqli_num_rows($sql_obat) > 0) {
                     while ($data = mysqli_fetch_array($sql_obat)) { ?>
                         <tr>
