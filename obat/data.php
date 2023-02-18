49e5ff672dd9df6b82c80ec24c145901be71138b
wisnawa <putu.wisnawa@gmail.com>
2023-02-18 11:35:10 +0800

    modified code in line 114 file data.php

--- obat/data.php
+++ obat/data.php
@@ -111,10 +111,9 @@
                     if ($i != $hal) {
                         echo "<li class=\"page-item\" ><a class=\"page-link\" href=\"?hal=$i\">$i</a></li>";
                     } else { ?>
-                        <!-- <span class="badge text-bg-primary"><?= $i; ?></span> -->
-                        <li class="page-item active">
+                        <!-- <li class="page-item active">
                             <span class="page-link"><?= $i; ?></span>
-                        </li>
+                        </li> -->
                 <?php }
                 }
                 ?>
