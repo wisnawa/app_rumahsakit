121afaaa7446ce7f162cbddb850fba8dc7704244
wisnawa <putu.wisnawa@gmail.com>
2023-02-23 14:58:27 +0800

    modified file proses.php in folder pasien

--- pasien/proses.php
+++ pasien/proses.php
@@ -24,7 +24,7 @@ if (isset($_POST['add'])) {
     // function for validation identitas (id tidak boleh sama)
     $sql_cek_identitas = mysqli_query($con, "SELECT * FROM tb_pasien WHERE `nomor_identitas` = '$identitas'") or die(mysqli_error($con));
     if (mysqli_num_rows($sql_cek_identitas) > 0) {
-        echo "<script>alert('Nomor identitas sudah ada yang menggunakan!');window.location='add.php';</script>";
+        echo "<script>alert('Nomor identitas ini sudah ada yang menggunakan!');window.location='add.php';</script>";
     } else {
         mysqli_query($con, "INSERT INTO tb_pasien (`id_pasien`, `nomor_identitas`, `nama_pasien`, `jenis_kelamin`, `alamat`, `no_telp`) VALUES ('$uuid', '$identitas', '$nama', '$jk', '$alamat', '$telp')") or die(mysqli_error($con));
         echo "<script>window.location='data.php';</script>";
